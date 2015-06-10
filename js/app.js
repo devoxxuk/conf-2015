6bcdbddab3aa7382a3c9cc40945ca9fcdd8c0ada
Jon Bevan <jbevan@adaptavist.com>
2015-06-10 11:03:44 +0100

    Fix component keys, apply column classes for media queries

--- js/app.js
+++ js/app.js
@@ -1,69 +1,87 @@
-var TalksContainer = React.createClass({displayName: "TalksContainer",
-    getInitialState: function() {
-        return {
-            loadingTalks: this.props.loadingTalks === "true" || false,
-            error: this.props.error || "",
-            talks: []
-        }
-    },
-    componentWillReceiveProps: function(nextProps) {
-        this.setState(nextProps);
-    },
-    render: function() {
-        var loadingTalks = this.state.loadingTalks ? React.createElement("div", {className: "alert alert-warning", id: "loading-talks-notification"}, "Loading talks...") : '';
-        var error = this.state.error === '' ? '' : React.createElement("div", {className: "alert alert-danger", id: "error-notification"}, this.state.error); 
-        var loaded = this.state.loadingTalks === false && this.state.error === '';
-        var table = loaded ? React.createElement(Talks, {details: this.state.talks}) : '';
-        return (
-          React.createElement("div", {key: "devoxx-top-talks-container"}, 
-            loadingTalks, 
-            error, 
-            table
-          )
-        );
-    }
-});
-
-var Talks = React.createClass({displayName: "Talks",
-    render: function(){
-      var talks = _.map(this.props.details, function(talk, idx){
-        return (
-          React.createElement("tr", {key: 'devoxx-talk-' + talk.name}, 
-            React.createElement("td", null, parseInt(idx) + 1), 
-            React.createElement("td", null, talk.title), 
-            React.createElement("td", null, talk.speakers.join(', ')), 
-            React.createElement("td", null, talk.type), 
-            React.createElement("td", null, talk.track), 
-            React.createElement("td", null, Math.round(talk.avg * 10)/10), 
-            React.createElement("td", null, talk.count)
-          )
-        );
-      });
-      return (
-        React.createElement("table", {className: "table table-striped", key: "devoxx-top-talks"}, 
-          React.createElement("thead", null, 
-            React.createElement("tr", null, 
-              React.createElement("th", null, "#"), 
-              React.createElement("th", null, "Title"), 
-              React.createElement("th", null, "Speakers"), 
-              React.createElement("th", null, "Talk Type"), 
-              React.createElement("th", null, "Track"), 
-              React.createElement("th", null, "Avg Vote"), 
-              React.createElement("th", null, "# Votes")
-            )
-          ), 
-          React.createElement("tbody", null, 
-            talks
-          )
-        )
-      );
-    }
-});
-
-
-var app = React.render(React.createElement(TalksContainer, {loadingTalks: "true"}), document.getElementById('main'));
-
 (function(){
+    var TalksContainer = React.createClass({displayName: "TalksContainer",
+        getInitialState: function() {
+            return {
+                loadingTalks: this.props.loadingTalks === "true" || false,
+                error: this.props.error || "",
+                talks: []
+            }
+        },
+        componentWillReceiveProps: function(nextProps) {
+            this.setState(nextProps);
+        },
+        render: function() {
+            var loadingTalks = this.state.loadingTalks ? React.createElement("div", {className: "alert alert-warning", id: "loading-talks-notification"}, "Loading talks...") : '';
+            var error = this.state.error === '' ? '' : React.createElement("div", {className: "alert alert-danger", id: "error-notification"}, this.state.error); 
+            var loaded = this.state.loadingTalks === false && this.state.error === '';
+            var table = loaded ? React.createElement(Talks, {details: this.state.talks, key: "devoxx-top-talks"}) : '';
+            return (
+              React.createElement("div", null, 
+                loadingTalks, 
+                error, 
+                table
+              )
+            );
+        }
+    });
+
+    var Talks = React.createClass({displayName: "Talks",
+        render: function(){
+          var talks = _.map(this.props.details, function(talk, idx){
+            return React.createElement(Talk, {rowNum: idx, details: talk, key: 'devoxx-talk-' + talk.name});
+          });
+          return (
+            React.createElement("table", {className: "table table-striped"}, 
+              React.createElement("thead", null, 
+                React.createElement("tr", null, 
+                  React.createElement("th", null, "#"), 
+                  React.createElement("th", null, "Title"), 
+                  React.createElement("th", null, "Speakers"), 
+                  React.createElement("th", {className: "devoxx-talk-type"}, "Talk Type"), 
+                  React.createElement("th", {className: "devoxx-track"}, "Track"), 
+                  React.createElement("th", null, "Avg Vote"), 
+                  React.createElement("th", {className: "devoxx-num-votes"}, "# Votes")
+                )
+              ), 
+              React.createElement("tbody", null, 
+                talks
+              )
+            )
+          );
+        }
+    });
+
+    var Talk = React.createClass({displayName: "Talk",
+        getInitialState: function(){
+            return {
+                rowNum: this.props.rowNum,
+                details: this.props.details,
+                className: ''
+            };
+        },
+        componentWillReceiveProps: function(nextProps) {
+            this.setState(nextProps);
+        },
+        render: function(){
+            var talk = this.state.details,
+                idx = this.state.rowNum;
+            return (
+              React.createElement("tr", {className: this.state.className}, 
+                React.createElement("td", null, parseInt(idx) + 1), 
+                React.createElement("td", null, talk.title), 
+                React.createElement("td", null, talk.speakers.join(', ')), 
+                React.createElement("td", {className: "devoxx-talk-type"}, talk.type), 
+                React.createElement("td", {className: "devoxx-track"}, talk.track), 
+                React.createElement("td", null, Math.round(talk.avg * 10)/10), 
+                React.createElement("td", {className: "devoxx-num-votes"}, talk.count)
+              )
+            );
+        }
+    });
+
+
+    var app = React.render(React.createElement(TalksContainer, {loadingTalks: "true", key: "devoxx-top-talks-container"}), document.getElementById('main'));
+
     var TOP_TALKS_URL = 'http://api.vote.devoxx.co.uk/duk15/top/talks?limit=10';
 
     function render(data) {
@@ -76,7 +94,7 @@ var app = React.render(React.createElement(TalksContainer, {loadingTalks: "true"
     }
 
     function refresh(){
-        setTimeout(getTalks, 5000);
+        setTimeout(getTalks, 10*1000);
     }
 
     function getTalks(){
--- src/app.jsx
+++ src/app.jsx
@@ -1,69 +1,87 @@
-var TalksContainer = React.createClass({
-    getInitialState: function() {
-        return {
-            loadingTalks: this.props.loadingTalks === "true" || false,
-            error: this.props.error || "",
-            talks: []
-        }
-    },
-    componentWillReceiveProps: function(nextProps) {
-        this.setState(nextProps);
-    },
-    render: function() {
-        var loadingTalks = this.state.loadingTalks ? <div className="alert alert-warning" id="loading-talks-notification">Loading talks...</div> : '';
-        var error = this.state.error === '' ? '' : <div className="alert alert-danger" id="error-notification">{this.state.error}</div>; 
-        var loaded = this.state.loadingTalks === false && this.state.error === '';
-        var table = loaded ? <Talks details={this.state.talks} /> : '';
-        return (
-          <div key="devoxx-top-talks-container">
-            {loadingTalks}
-            {error}
-            {table}
-          </div>
-        );
-    }
-});
-
-var Talks = React.createClass({
-    render: function(){
-      var talks = _.map(this.props.details, function(talk, idx){
-        return (
-          <tr key={'devoxx-talk-' + talk.name}>
-            <td>{parseInt(idx) + 1}</td>
-            <td>{talk.title}</td>
-            <td>{talk.speakers.join(', ')}</td>
-            <td>{talk.type}</td>
-            <td>{talk.track}</td>
-            <td>{Math.round(talk.avg * 10)/10}</td>
-            <td>{talk.count}</td>
-          </tr>
-        );
-      });
-      return (
-        <table className="table table-striped" key="devoxx-top-talks">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Title</th>
-              <th>Speakers</th>
-              <th>Talk Type</th>
-              <th>Track</th>
-              <th>Avg Vote</th>
-              <th># Votes</th>
-            </tr>
-          </thead>
-          <tbody>
-            {talks}
-          </tbody>
-        </table>
-      );
-    }
-});
-
-
-var app = React.render(<TalksContainer loadingTalks="true" />, document.getElementById('main'));
-
 (function(){
+    var TalksContainer = React.createClass({
+        getInitialState: function() {
+            return {
+                loadingTalks: this.props.loadingTalks === "true" || false,
+                error: this.props.error || "",
+                talks: []
+            }
+        },
+        componentWillReceiveProps: function(nextProps) {
+            this.setState(nextProps);
+        },
+        render: function() {
+            var loadingTalks = this.state.loadingTalks ? <div className="alert alert-warning" id="loading-talks-notification">Loading talks...</div> : '';
+            var error = this.state.error === '' ? '' : <div className="alert alert-danger" id="error-notification">{this.state.error}</div>;
+            var loaded = this.state.loadingTalks === false && this.state.error === '';
+            var table = loaded ? <Talks details={this.state.talks} key="devoxx-top-talks" /> : '';
+            return (
+              <div>
+                {loadingTalks}
+                {error}
+                {table}
+              </div>
+            );
+        }
+    });
+
+    var Talks = React.createClass({
+        render: function(){
+          var talks = _.map(this.props.details, function(talk, idx){
+            return <Talk rowNum={idx} details={talk} key={'devoxx-talk-' + talk.name} />;
+          });
+          return (
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Title</th>
+                  <th>Speakers</th>
+                  <th className="devoxx-talk-type">Talk Type</th>
+                  <th className="devoxx-track">Track</th>
+                  <th>Avg Vote</th>
+                  <th className="devoxx-num-votes"># Votes</th>
+                </tr>
+              </thead>
+              <tbody>
+                {talks}
+              </tbody>
+            </table>
+          );
+        }
+    });
+
+    var Talk = React.createClass({
+        getInitialState: function(){
+            return {
+                rowNum: this.props.rowNum,
+                details: this.props.details,
+                className: ''
+            };
+        },
+        componentWillReceiveProps: function(nextProps) {
+            this.setState(nextProps);
+        },
+        render: function(){
+            var talk = this.state.details,
+                idx = this.state.rowNum;
+            return (
+              <tr className={this.state.className}>
+                <td>{parseInt(idx) + 1}</td>
+                <td>{talk.title}</td>
+                <td>{talk.speakers.join(', ')}</td>
+                <td className="devoxx-talk-type">{talk.type}</td>
+                <td className="devoxx-track">{talk.track}</td>
+                <td>{Math.round(talk.avg * 10)/10}</td>
+                <td className="devoxx-num-votes">{talk.count}</td>
+              </tr>
+            );
+        }
+    });
+
+
+    var app = React.render(<TalksContainer loadingTalks="true" key="devoxx-top-talks-container" />, document.getElementById('main'));
+
     var TOP_TALKS_URL = 'http://api.vote.devoxx.co.uk/duk15/top/talks?limit=10';
 
     function render(data) {
@@ -76,7 +94,7 @@ var app = React.render(<TalksContainer loadingTalks="true" />, document.getEleme
     }
 
     function refresh(){
-        setTimeout(getTalks, 5000);
+        setTimeout(getTalks, 10*1000);
     }
 
     function getTalks(){
